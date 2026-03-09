db5139b736b8e7af6be9618de286f465a535e7b8
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2026-03-09 20:24:11 +0200

    adding  system features

---
+++ ZooManagementSystem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ZooManagementSystem {
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // FEATURE 1
+        System.out.println("Welcome to the Zoo Management System");
+        System.out.println("1. View Animals");
+        System.out.println("2. Search Animals");
+        System.out.println("4. Exit");
+        // Scanner scanner input
+
+        // ADD ANIMAL
+        //ENTER ANIMAL NAME
+        System.out.println("Enter animal name: ");
+        String animalName = scanner.nextLine();
+        
+        //ENTER ANIMAL SPECIES
+        System.out.println("Enter animal species: ");
+        String animalSpecies = scanner.nextLine();
+
+        // STORE THE INFORMATION IN A SYSTEM
+
+    }
+    
+}
