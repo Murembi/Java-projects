c5595523b913cdda0e8746e0e6d36b291671e097
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2026-03-10 20:05:03 +0200

    add array on animal species and names

--- ZooManagementSystem.java
+++ ZooManagementSystem.java
@@ -4,11 +4,18 @@ public class ZooManagementSystem {
     
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+
+        // declaring variables 
+        String[] animalNames= new String[20];  // an empty array
+        String[] animalSpecies= new String[20];
+
         int choice = 0;
+        int animalCount = 0; //tracks how many animals exists
+
+        System.out.println("Welcome to the Zoo Management System");
         // add a while loop
         while(choice != 3){
         // FEATURE 1
-        System.out.println("Welcome to the Zoo Management System");
         System.out.println("menu");
         System.out.println("1. View Animals");
         System.out.println("2. Search Animals");
@@ -20,9 +27,16 @@ public class ZooManagementSystem {
         switch(choice){
             case 1: 
                 System.out.println("Animals in the zoo: ");
-                System.out.println("Lion");
-                System.out.println("Elephant");
-                System.out.println("Giraffe");
+
+                // user selects view animals
+                if(animalCount == 0){
+                    System.out.println("No animals added yet.");
+                }else{
+                    for(int i = 0; i < animalCount; i++){
+                        // print animals names and species that exists
+                        System.out.println(animalNames[i] + " - " + animalSpecies[i]);
+                    }
+                }
                 break;
 
             case 2:
@@ -33,17 +47,18 @@ public class ZooManagementSystem {
 
                 System.out.println("Enter animal name");
                 String name = scanner.nextLine();
+                animalNames[animalCount] = name; //store name 
 
-                System.out.println("Enter animal type: ");
-                String type = scanner.nextLine();
+                System.out.println("Enter animal species: ");
+                String species = scanner.nextLine();
+                animalSpecies[animalCount]=species; // store animal species
 
-                System.out.println("Enter animal age: ");
-                int age = scanner.nextInt();
-                    System.out.println("\nAnimal Added:");
+                animalCount++;
+
+                System.out.println("\nAnimal Added:");
                 
                 System.out.println("Name: " + name);
-                System.out.println("Type: " + type);
-                System.out.println("Age: " + age);
+                System.out.println("Species " + species);
 
                 break;
                 
@@ -57,9 +72,7 @@ public class ZooManagementSystem {
         }
 
         }
-        // PART 1 COMPLETED
-
-        // PART 2
+        
 
 
     }
