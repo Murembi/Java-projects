c58454e9527d4dde0031374cc84164bc2ae12839
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2026-03-10 06:25:09 +0200

    program setup

--- ZooManagementSystem.java
+++ ZooManagementSystem.java
@@ -7,10 +7,54 @@ public class ZooManagementSystem {
 
         // FEATURE 1
         System.out.println("Welcome to the Zoo Management System");
+        System.out.println("menu");
         System.out.println("1. View Animals");
         System.out.println("2. Search Animals");
         System.out.println("4. Exit");
+        int choice = scanner.nextInt();
         // Scanner scanner input
+
+        switch(choice){
+            case 1: 
+                System.out.println("Animals in the zoo: ");
+                System.out.println("Lion");
+                System.out.println("Elephant");
+                System.out.println("Giraffe");
+                break;
+
+            case 2:
+                System.out.println("Add animal selected:");
+                scanner.nextLine();
+                
+                // Storing data in variables
+
+                System.out.println("Enter animal name");
+                String name = scanner.nextLine();
+
+                System.out.println("Enter animal type: ");
+                String type = scanner.nextLine();
+
+                System.out.println("Enter animal age: ");
+                int age = scanner.nextInt();
+                    System.out.println("\nAnimal Added:");
+                
+                System.out.println("Name: " + name);
+                System.out.println("Type: " + type);
+                System.out.println("Age: " + age);
+
+                break;
+                
+            
+            case 3:
+                System.out.println("Exiting program...");
+                break;
+            
+            default:
+                System.out.println("Invalid option");
+
+        }
+
+
 
         // ADD ANIMAL
         //ENTER ANIMAL NAME
@@ -22,6 +66,7 @@ public class ZooManagementSystem {
         String animalSpecies = scanner.nextLine();
 
         // STORE THE INFORMATION IN A SYSTEM
+
 
     }
     
