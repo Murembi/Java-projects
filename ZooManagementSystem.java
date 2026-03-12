3ff6df239246e687e5f967f69378afa532c42bce
Murembiwa Mutswaletswale <murembiwanoz64@gmail.com>
2026-03-12 04:17:51 +0200

    added a new menu option to search animals

--- ZooManagementSystem.java
+++ ZooManagementSystem.java
@@ -9,17 +9,20 @@ public class ZooManagementSystem {
         String[] animalNames= new String[20];  // an empty array
         String[] animalSpecies= new String[20];
 
-        int choice = 0;
+        int choice = 0; //declaring choice
         int animalCount = 0; //tracks how many animals exists
 
         System.out.println("Welcome to the Zoo Management System");
+
         // add a while loop
-        while(choice != 3){
+        while(choice != 4){
+
         // FEATURE 1
         System.out.println("menu");
         System.out.println("1. View Animals");
-        System.out.println("2. Search Animals");
-        System.out.println("3. Exit");
+        System.out.println("2. Add Animals");
+        System.out.println("3. Search Animal"); //search for an animal 
+        System.out.println("4. Exit");
         choice = scanner.nextInt();
         // Scanner scanner input
 
@@ -53,7 +56,7 @@ public class ZooManagementSystem {
                 String species = scanner.nextLine();
                 animalSpecies[animalCount]=species; // store animal species
 
-                animalCount++;
+                animalCount++; //incremeant the count after adding
 
                 System.out.println("\nAnimal Added:");
                 
@@ -61,9 +64,31 @@ public class ZooManagementSystem {
                 System.out.println("Species " + species);
 
                 break;
-                
-            
+
             case 3:
+                System.out.println("Enter animal name to search: ");
+                String searchedAnimal = scanner.nextLine();
+
+                boolean found = false;
+
+                for(int i =0; i<animalNames.length; i++){
+                    if(animalNames[i].equals(searchedAnimal)){
+                        System.out.println("Animal found");
+                        System.out.println("Name: " + animalNames[i]);
+                        System.out.println("Species: " + animalSpecies[i]);
+
+                        found = true;
+                    }
+
+                }
+                
+                if(!found) {
+                    System.out.println("Animal not found");
+                } 
+                
+                break;
+            
+            case 4:
                 System.out.println("Exiting program...");
                 break;
             
